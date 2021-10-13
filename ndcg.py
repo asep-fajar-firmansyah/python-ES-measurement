9297e9df169e0e4ab4f78cf75b276609d6518e83
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2021-10-13 08:44:38 +0200

    new file:   ndcg.py

---
+++ ndcg.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 18 10:19:58 2021
+
+@author: asep
+"""
+
+import math
+
+class NDCG:
+    def _getScore(self, tripleGoldSummaries, triplesRank):
+        tripleGrade = {}
+        for tripleGoldSum in tripleGoldSummaries:
+            for t in tripleGoldSum:
+                if t not in tripleGrade:
+                    tripleGrade[t]=1
+                else:
+                    tripleGrade[t]= tripleGrade[t]+1
+        gradeList = list(tripleGrade.values())
+        gradeList.sort(reverse=True)
+        
+        dcg = 0
+        idcg = 0
+        
+        maxRankPos = len(triplesRank)
+        maxIdealPos = len(gradeList)
+        
+        for pos in range(1, maxRankPos+1):
+            t = triplesRank[pos-1]
+            #print("t", t)
+            try:
+                rel = tripleGrade[t]
+            except:
+                rel=0
+            dcgItem = rel/math.log(pos + 1, 2)
+            dcg += dcgItem
+            
+            if (pos<=maxIdealPos):
+                idealRel = gradeList[pos-1]
+                #print("ideal", idealRel)
+                idcg += idealRel/math.log(pos + 1, 2)
+        
+        score = dcg/idcg
+        return score
