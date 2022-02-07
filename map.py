2b51518085453c6e870f5a4f808d0c0b4de571db
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-02-07 12:41:57 +0100

    Fix warnings on MAP module

--- map.py
+++ map.py
@@ -5,7 +5,7 @@ Created on Wed Dec 15 11:35:58 2021
 
 @author: asep
 
-reference from 
+reference from\ 
 [1] https://github.com/nju-websoft/ESBM/tree/master/v1.2
 [2] https://github.com/nju-websoft/DeepLENS/blob/master/code/train_test.py
 """
@@ -16,20 +16,19 @@ class MAP:
         sum_f=0
         u_num = len(gold_summ_list)
         for gold_summ in gold_summ_list:
-            map_score = self.getMAP(summ_tids, gold_summ)
+            map_score = self.get_map(summ_tids, gold_summ)
             sum_f += map_score
         avg_map = sum_f/u_num
         return avg_map
-    def get_MAP(self, summ_tids, gold_summ):
+    def get_map(self, summ_tids, gold_summ):
         avg_p=0
         result_size = len(summ_tids)
         correct_size = 0
         for i in range(1, result_size+1):
             if summ_tids[i-1] in gold_summ:
-                p_scores = self._getPrecisionScore(summ_tids[:i], gold_summ)
+                p_scores = self.get_precision_score(summ_tids[:i], gold_summ)
                 correct_size +=1
                 avg_p += p_scores
-        
         if correct_size != 0:
             avg_pr = avg_p/len(gold_summ)
         else:
